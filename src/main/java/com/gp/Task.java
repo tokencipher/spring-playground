4c89aeec10af228477c3c22ff267c6f0f8866631
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2019-08-14 10:13:22 -0400

    Create class to be instantiated with inflated path variable

--- src/main/java/com/gp/Task.java
+++ src/main/java/com/gp/Task.java
@@ -1,4 +1,15 @@
 package com.gp;
 
 public class Task {
+  private int taskId;
+  private int commentId;
+
+  public int getTaskId() { return taskId; }
+
+  public void setTaskId(int taskId) { this.taskId = taskId; }
+
+  public int getCommentId() { return commentId; }
+
+  public void setCommentId(int commentId) { this.commentId = commentId; }
+
 }
