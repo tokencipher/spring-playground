5ffc87c1b8ead031caa9431717ead20acb8bf694
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2019-08-15 13:03:09 -0400

    Capitalize first letter in comments for consistency

--- src/main/java/com/gp/PagesController.java
+++ src/main/java/com/gp/PagesController.java
@@ -50,26 +50,26 @@ public class PagesController {
     return type;
   }
 
-  // how to access querystring params
+  // How to access querystring params
   @GetMapping("/tasks")
   public String getTaskInfo(@RequestParam String filter) {
     return String.format("The filter for your task is: %s \n", filter);
   }
 
 
-  // what if the name of the querystring is not a valid Java parameter name?
+  // What if the name of the querystring is not a valid Java parameter name?
   @GetMapping("/people")
   public String getPeople(@RequestParam("sort-by") String sortBy, @RequestParam(value = "sort-dir") String sortDir) {
     return String.format("sortBy is %s and sortDirection is %s", sortBy, sortDir);
   }
 
-  // access querystring data as a map
+  // Access querystring data as a map
   @GetMapping("/map-example")
   public String getMapParams(@RequestParam Map querystring) {
     return querystring.toString();
   }
 
-  // access querystring inflated into an custom object
+  // Access querystring inflated into an custom object
   @GetMapping("/object-example")
   public String getObjectParams(CarInfo carInfo) {
     return String.format("Your car's make is %s and model is %s", carInfo.getMake(), carInfo.getModel());
